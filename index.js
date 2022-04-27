5289ecda1f9a0ee35006754fc93eabebca8da040
oerdbej <oerdbej@gmail.com>
2022-04-27 22:54:00 +0200

    filter method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 
 const bootcamp = require("./bootcamp");
+const json = require("./json");
 
 app.get("/", (req, res) => {
   console.log("Hello from terminal");
@@ -12,6 +13,16 @@ app.get("/", (req, res) => {
 
 app.get("/bootcamp", (req, res) => {
   res.json(bootcamp);
+});
+
+app.get("/json", (req, res) => {
+  res.json(json);
+  console.log("hello from backend json");
+});
+
+app.get("/bootcamp/students/under22", (req, res) => {
+  let youngStudents = bootcamp.students.filter((element) => element.age < 23);
+  res.json(youngStudents);
 });
 
 app.listen(port, () => console.log(`Server is running from the port ${port}`));
---
+++ json.js
@@ -0,0 +1,40 @@
+const json = {
+  students: [
+    {
+      name: "Oerd",
+      living: "Tirana",
+      background: "Marketing",
+    },
+    {
+      name: "Moran",
+      living: "Berlin",
+      background: "Personal Trainer",
+    },
+    {
+      name: "Arne",
+      living: "Bavaria",
+      background: "Researcher",
+    },
+    {
+      name: "Billy",
+      living: "Paris",
+      background: "Mechanic",
+    },
+  ],
+  WildeSchool: [
+    {
+      campus: "Berlin",
+      state: "Germany",
+    },
+    {
+      campus: "Amsterdam",
+      state: "Holand",
+    },
+    {
+      campus: "Barcelona",
+      state: "Spain",
+    },
+  ],
+};
+
+module.exports = json;
