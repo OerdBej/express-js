f9644572a415a94b5e2379f10b215d09c0c21b4a
oerdbej <oerdbej@gmail.com>
2022-04-27 22:27:18 +0200

    passing a json

---
+++ bootcamp.js
@@ -0,0 +1,36 @@
+const bootcamp = {
+  students: [
+    {
+      name: "Oerd",
+      living: "Tirana",
+      age: 23,
+    },
+    {
+      name: "Billy",
+      living: "France",
+      age: 23,
+    },
+    {
+      name: "Arne",
+      living: "Berlin",
+      age: 24,
+    },
+    {
+      name: "Kenneth",
+      living: "France",
+      age: 21,
+    },
+  ],
+  instructors: [
+    {
+      name: "Marc",
+      living: "Barcelona",
+    },
+    {
+      name: "Luke",
+      living: "Dogland",
+    },
+  ],
+};
+
+module.exports = bootcamp;
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+const bootcamp = require("./bootcamp");
+
+app.get("/", (req, res) => {
+  console.log("Hello from terminal");
+  res.send("Hello DOM");
+});
+
+app.get("/bootcamp", (req, res) => {
+  res.json(bootcamp);
+});
+
+app.listen(port, () => console.log(`Server is running from the port ${port}`));
